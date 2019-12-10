b17e164a780f80b364eab282fa808557b22fefae
AnkitSakhiya <sakhiyaankit7@gmail.com>
2019-12-10 13:31:27 +0530

    Added Angular material and added one component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TaskStatusComponent } from './task-status/task-status.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, DragDropModule ],
+  declarations: [ AppComponent, HelloComponent, TaskStatusComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
